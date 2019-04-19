f02f55adb02c3ab8f1ef20f4ce689e81e10b2704
Robert McCary <robert.mccary@yahoo.com>
2019-04-19 18:53:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function lowerCaseDrivers(drivers){
 
 function nameToAttributes(drivers){
   return drivers.map(function(driver){
-    const firstName = driver.split('')[0];
-    const lastName = driver.split('')[1];
+    const firstName = driver.split(' ')[0];
+    const lastName = driver.split(' ')[1];
 
       return {firstName: firstName, lastName: lastName };
   });
